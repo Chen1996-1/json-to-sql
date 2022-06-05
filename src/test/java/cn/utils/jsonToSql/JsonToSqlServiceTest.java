0367f3eda15aa40587f80768d0ad1200207db4f1
Chen1996-1 <2435740021@qq.com>
2022-06-05 19:09:33 +0800

    init

--- src/test/java/cn/utils/jsonToSql/JsonToSqlServiceTest.java
+++ src/test/java/cn/utils/jsonToSql/JsonToSqlServiceTest.java
@@ -1,9 +1,21 @@
 package cn.utils.jsonToSql;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Resource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class JsonToSqlServiceTest {
 
+public class JsonToSqlServiceTest {
+
+    private JsonToSqlService jsonToSqlService = new JsonToSqlServiceImpl();
+    @Test
     public void testJsonToSql() {
+        JsonToSqlInfo jsonToSqlInfo = new JsonToSqlInfo();
+        jsonToSqlInfo.setFilePath("F:\\Desktop\\tem.json");
+        String[] sqlArray  = jsonToSqlService.JsonToSql(jsonToSqlInfo);
+        System.out.println("ok");
     }
 }
